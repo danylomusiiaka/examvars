ea4b1e9a510d87c590c10a5c7ba1fe99e81bf4ce
Danylo Musiiaka <dollarone98@gmail.com>
2022-12-20 21:57:55 +0200

    added fibonachi(2) & var8

---
+++ var8ex3.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+    string s;
+    cout << "Enter string: ";
+    getline(cin, s);
+    int slength = s.length();
+    int pos1;
+    int pos = 0;
+    int _slivf = 0;
+    int _slivj = 0;
+    do {
+        pos1 = s.find(" ", pos);
+        if (pos1 == -1) {
+            pos1 = s.find(".", pos);
+        }
+        if (s[pos1 - 1] == 'f') {
+            s.erase(pos, pos1 - pos);
+            pos1 = pos - 1;
+            slength = s.length();
+            _slivf += 1;
+            pos = pos1 + 1;
+        }
+        if (s[pos1 - 1] == 'j') {
+            s.erase(pos, pos1 - pos);
+            pos1 = pos - 1;
+            slength = s.length();
+            _slivj += 1;
+        }
+        pos = pos1 + 1;
+    } while (pos != slength);
+
+    cout << "Result is: " << s << endl;
+    cout << "word with f:" << _slivf << endl;
+    cout << "word with j:" << _slivj << endl;
+    return 0;
+}
